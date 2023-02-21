69f741d94b903f51956b9b0e0bedf4b1bc3290f1
osawa-koki <github@osawa.dev>
2023-02-21 20:58:52 +0900

    小さな修正。

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "azurerm_resource_group" "resource_group" {
 
 // https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/function_app
 resource "azurerm_storage_account" "storage_account" {
-  name = var.storage_account_name
+  name = replace(var.storage_account_name, "-", "")
   resource_group_name = azurerm_resource_group.resource_group.name
   location = azurerm_resource_group.resource_group.location
   account_tier = "Standard"
